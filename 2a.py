5a63134abcb10b17adfe63343758c91fc69e21ec
Ishay Post <ishaypo@edu.hac.ac.il>
2023-02-27 15:03:15 +0200

    2a half work

--- 2a.py
+++ 2a.py
@@ -7,27 +7,32 @@ from matplotlib import pyplot as plt
 import math
 import os
 
+sp = 1
 
 def in_bounds(i,j,image):
-    return i in range(len(image[0])) and j in range(len(image))
+    return i in range(len(image)) and j in range(len(image[0]))
+
+def find_same(x_pix,y_pix, t_im,f_im):
+    same = [x_pix,y_pix]
+    #print(x_pix, y_pix)
+    mn = t_im[x_pix][y_pix] + 254
+    for i in range(y_pix - sp, y_pix + sp):
+        for j in range(x_pix - sp, x_pix + sp):
+           # mn = f_im[x_pix][y_pix]
+            if in_bounds(i,j, t_im):
+                new = new_min(x_pix,y_pix, i, j, mn, t_im ,f_im)
+                if new < mn:
+                    same = [i,j]
+                    mn = new
+    return same
 
 def new_min(x_pix,y_pix, i, j, mn, t_im ,f_im):
-    print(t_im[x_pix][y_pix], f_im[i][j], sep='\n')
-
     new = abs(t_im[x_pix][y_pix] - f_im[i][j])
     if new < mn:
         return new
     return mn
 
-def find_same(x_pix,y_pix, t_im,f_im):
-    same = [x_pix,y_pix]
-    for i in range(y_pix - 5, y_pix + 5):
-        for j in range(x_pix - 5, x_pix + 5):
-            mn = f_im[i][j]
-            if in_bounds(i,j, to_image):
-                new = new_min(i,j,mn)
-                if new != mn:
-                    same = [i,j]
+
 
 
 persons = ['cgboyc', 'cmkirk', 'djhugh','dmwest', 'gmwate','khughe','lejnno']
@@ -44,35 +49,29 @@ fromName = 'cgboyc' + '.' + str(16) + '.jpg'
 to_image = Image.open(toName).convert('L')
 from_image = Image.open(fromName)
 gray_from_image = Image.open(fromName).convert('L')
+new_to = Image.open(toName)
+
+to_values = np.asarray(to_image,dtype='int64').copy()
+from_values = np.asarray(from_image,dtype='int64').copy()
+gray_from_values = np.asarray(gray_from_image,dtype='int64').copy()
+new_values = np.asarray(new_to).copy()
 
 
+for i in range(len(to_values)):
+    for j in range(len(to_values[0])):
+        x,y = find_same(i,j,to_values,gray_from_values)
+        #print(x,y, type(to_values[i][j]),type(from_values[x][y]))
+        new_values[i][j] = from_values[x][y]
+
+#
+# plt.imshow(to_image)
+# plt.show()
 
 
+new_image = Image.fromarray(new_values)
+plt.imshow(new_image)
+plt.show()
 
-im = [[1,2,3],
-      [4,5,6],
-      [7,8,9]]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# pixel_values = np.asarray(image)
-# pixel_values = pixel_values.copy()
 
 
 
