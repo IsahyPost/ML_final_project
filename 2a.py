8f79425297ceb2a21f957fce9ae1c122591fe863
Ishay Post <ishaypo@edu.hac.ac.il>
2023-02-28 00:47:41 +0200

    2a half work and A bit neat

--- 2a.py
+++ 2a.py
@@ -7,7 +7,9 @@ from matplotlib import pyplot as plt
 import math
 import os
 
-sp = 1
+
+
+sp = 5
 
 def in_bounds(i,j,image):
     return i in range(len(image)) and j in range(len(image[0]))
@@ -26,27 +28,50 @@ def find_same(x_pix,y_pix, t_im,f_im):
                     mn = new
     return same
 
+def similarity(x_pix,y_pix, i, j, t_im ,f_im):
+    return abs(t_im[x_pix][y_pix] - f_im[i][j])
+
+def distance(x_pix,y_pix, i, j):
+    return math.sqrt((x_pix - i) ** 2 + (y_pix - j) ** 2)
+
 def new_min(x_pix,y_pix, i, j, mn, t_im ,f_im):
-    new = abs(t_im[x_pix][y_pix] - f_im[i][j])
+    dis = (distance(x_pix,y_pix, i, j))
+    sim = (similarity(x_pix,y_pix, i, j, t_im ,f_im))
+    new = abs( sim +  dis)
+    #new = abs(t_im[x_pix][y_pix] - f_im[i][j])
     if new < mn:
         return new
     return mn
+
+def plot_results(new_values, org_image, gray_from_image):
+    new_image = Image.fromarray(new_values)
+
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 5))
+
+    # Display the images on the subplots
+    # ax1.imshow(org_image, cmap='gray')
+    ax1.imshow(org_image)
+    ax2.imshow(new_image)
+    ax3.imshow(gray_from_image, cmap='gray')
+    # Set titles for the subplots
+    ax1.set_title('original')
+    ax2.set_title('new')
+    ax3.set_title('gray original')
+
+    # Show the plot
+    plt.show()
 
 
 
 
 persons = ['cgboyc', 'cmkirk', 'djhugh','dmwest', 'gmwate','khughe','lejnno']
 
-# for name in persons:
-#     imageName = name + '.' + str(20) + '.jpg'
-#     # Open the image form working directorys
-#     image = Image.open(imageName).convert('L')
-#     plt.imshow(image, cmap='gray')
-#     plt.show()
+
 
 toName = 'cgboyc' + '.' + str(12) + '.jpg'
 fromName = 'cgboyc' + '.' + str(16) + '.jpg'
 to_image = Image.open(toName).convert('L')
+org_image = Image.open(toName)
 from_image = Image.open(fromName)
 gray_from_image = Image.open(fromName).convert('L')
 new_to = Image.open(toName)
@@ -54,23 +79,33 @@ new_to = Image.open(toName)
 to_values = np.asarray(to_image,dtype='int64').copy()
 from_values = np.asarray(from_image,dtype='int64').copy()
 gray_from_values = np.asarray(gray_from_image,dtype='int64').copy()
-new_values = np.asarray(new_to).copy()
 
 
-for i in range(len(to_values)):
-    for j in range(len(to_values[0])):
-        x,y = find_same(i,j,to_values,gray_from_values)
-        #print(x,y, type(to_values[i][j]),type(from_values[x][y]))
-        new_values[i][j] = from_values[x][y]
 
-#
-# plt.imshow(to_image)
-# plt.show()
+def restore_colors(to_values, gray_from_values, from_values):
+    new_values = np.asarray(new_to).copy()
+
+    for i in range(len(to_values)):
+        for j in range(len(to_values[0])):
+            x, y = find_same(i, j, to_values, gray_from_values)
+            new_values[i][j] = from_values[x][y]
+    return new_values
 
 
-new_image = Image.fromarray(new_values)
-plt.imshow(new_image)
-plt.show()
+
+nv = restore_colors(to_values, gray_from_values, from_values)
+plot_results(nv, org_image, gray_from_image)
+
+
+# max sim = 221
+# min sim = 0
+# max dis = 260.3113520382851
+# min dis = 0.0
+# len 12602475
+
+
+
+
 
 
 
